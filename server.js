175b7cb30c4f65fcb2a81a5f802ed0a96ce55000
SamuelInocencio <samuelferreirainocencio@gmail.com>
2024-12-10 15:11:07 -0300

    concluida aula 11 - Quero/Rout/Body Params

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
 
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios/:abacate', (req, res) => {
+    console.log(req)
     res.send("Olá, rota acessada com sucesso!!!")
 })
 
 app.post('/usuarios', (req, res) => {
+    console.log(req)
+
     res.send("Olá, aqui é uma rota post!!!")
 })
-
 
 
 
