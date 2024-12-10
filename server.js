e45b99f8c13d2fced69bc928bf7182cdf35d629f
SamuelInocencio <samuelferreirainocencio@gmail.com>
2024-12-10 14:15:31 -0300

    concluida aula 10 - criando primeira rota post

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/usuarios', (req, res) => {
     res.send("Olá, rota acessada com sucesso!!!")
 })
 
+app.post('/usuarios', (req, res) => {
+    res.send("Olá, aqui é uma rota post!!!")
+})
+
 
 
 
